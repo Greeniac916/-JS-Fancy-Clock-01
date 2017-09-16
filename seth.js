e4857f0f229d1c54da08c59412d63a8c330b3013
Greeniac916 <greeniac916@gmail.com>
2017-09-15 23:32:13 -0400

    Update seth.js

--- seth.js
+++ seth.js
@@ -1,2 +1,4 @@
-document.getElementById("chatmessage").value = "Die sloth";
-document.getElementsByClassName("btn_darkblue_white_innerfade btn_medium")[0].click();
+setInterval(function() {
+  document.getElementById("chatmessage").value = "Die sloth";
+  document.getElementsByClassName("btn_darkblue_white_innerfade btn_medium")[0].click();
+}, 100);
